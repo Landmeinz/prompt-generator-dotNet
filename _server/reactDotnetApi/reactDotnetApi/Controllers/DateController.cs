7404384369e699d0da47b9d8cc99d20271b07d2c
Eric Meinzer <emeinzer7@gmail.com>
2023-01-02 20:39:14 -0600

    updated server

--- _server/reactDotnetApi/reactDotnetApi/Controllers/DateController.cs
+++ _server/reactDotnetApi/reactDotnetApi/Controllers/DateController.cs
@@ -20,21 +20,21 @@ public class DateController : ControllerBase
         _dbContext = dbContext;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetDateTime()
-    {
+    //[HttpGet]
+    //public async Task<IActionResult> GetDateTime()
+    //{
         //var currentDateTime = await _dbContext.
         //    .Select("CURRENT_DATE");
 
         //var dQuery = _dbContext.CreateQuery<DateTime>("CurrentDateTime() ");
         //DateTime dbDate = dQuery.AsEnumerable().First();
 
-        var query = from cd in _dbContext.Keywords
-                    select "CURRENT_DATE";
+        //var query = from cd in _dbContext.Keywords
+        //            select "CURRENT_DATE";
 
-        var currentDateTime = query.First<DateTime>();
+        //var currentDateTime = query.First<DateTime>();
 
-        return Ok(currentDateTime);
-    }
+        //return Ok(currentDateTime);
+    //}
 }
 
